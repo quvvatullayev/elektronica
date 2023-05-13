be9d9613d2c024b9e3fd2f80c9688ae6dc1d88bc
ogabek <quvvatullayev@gmail.com>
2023-05-13 09:07:29 +0500

    create modle

--- elektronica/models.py
+++ elektronica/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Eliments(models.Model):
+    key = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.key
+    
+class Eliment_info(models.Model):
+    key = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    value = models.CharField(max_length=100)
+    formula = models.CharField(max_length=100)
+    eliment = models.ForeignKey(Eliments, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.key
