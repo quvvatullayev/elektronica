2a15acc5769543cc8c638e8d1ff51e432b9aa8e8
ogabek <quvvatullayev@gmail.com>
2023-05-13 09:49:30 +0500

    create eliments list

---
+++ elektronica/views/eliments.py
@@ -0,0 +1,16 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework import status
+from ..models import (
+    Eliments,
+)
+from ..serialization import (
+    ElimentsSerializer,
+)
+
+class ElimentsList(APIView):
+    def get(self, request:Request):
+        eliments = Eliments.objects.all()
+        serializer = ElimentsSerializer(eliments, many=True)
+        return Response(serializer.data)
