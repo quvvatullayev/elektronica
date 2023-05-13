c9b73d3571c7e285c72b024d09e027a9faf588d6
ogabek <quvvatullayev@gmail.com>
2023-05-13 09:10:22 +0500

    create eliment serialization

---
+++ elektronica/serialization.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from models import (
+    Eliments,
+    Eliment_info,
+)
+
+class ElimentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Eliments
+        fields = "__all__"
+
+class Eliment_infoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Eliment_info
+        fields = "__all__"
