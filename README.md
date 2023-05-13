2e698116186a7c0b299b7e65a123e3a51c8ede7b
ogabek <quvvatullayev@gmail.com>
2023-05-13 10:50:28 +0500

    add doc

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# elektronica
+# elektronica Documentation
+
+## `eliments api`
+
+| Method | path | description |
+| --- | --- | --- |
+| GET | `/eliments` | get all eliments |
+
+## `eliment info api`
+
+| Method | path | description |
+| --- | --- | --- |
+| GET | `/eliment_info/<eliment_id>` | get eliment info |
+| GET | `/eliment_info/` | get all eliment info |
--- elektronica/urls.py
+++ elektronica/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from .views.eliments import ElimentsList
-from .views.eliment_info import Eliment_infoList, Eliment_infoGet
 
 urlpatterns = [
     path('eliments/', ElimentsList.as_view()),
+]
+
+from .views.eliment_info import Eliment_infoList, Eliment_infoGet
+
+urlpatterns += [
     path('eliment_info/', Eliment_infoList.as_view()),
     path('eliment_info/<int:eliments_id>/', Eliment_infoGet.as_view()),
 ]
