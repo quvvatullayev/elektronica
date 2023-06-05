bc026d93375fc0b6c7a0ac672b7119680bea6ef3
ogabek <quvvatullayev@gmail.com>
2023-06-05 14:15:47 +0500

    create templates

--- elektronica/views/eliment_info.py
+++ elektronica/views/eliment_info.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework import status
+from django.shortcuts import render
 from ..models import (
     Eliment_info,
     Eliments,
@@ -21,15 +22,24 @@ class Eliment_infoGet(APIView):
     def get(self, request:Request, eliments_id:int):
         eliment_info = Eliment_info.objects.get(eliment=eliments_id)
         serializer = Eliment_infoSerializer(eliment_info)
-        return Response(serializer.data)
+        data = serializer.data
+        data['values'] = data['values'].split()
+        print(data)
+        return render(request, 'eliment_info.html', {'eliment_info': data})
     
 class ElimentsCount(APIView):
-    def get(self, request:Request, pk:int):
+    def post(self, request:Request, pk:int):
         data = request.data
+
         eliment = Eliment_info.objects.get(pk=pk)
         eliment_data = Eliment_infoSerializer(eliment).data
-        formula = eliment_data['formula_text']
 
+        eliment_info = Eliment_info.objects.get(id=pk)
+        serializer = Eliment_infoSerializer(eliment_info)
+
+        response = serializer.data
+        response['values'] = response['values'].split()
+        formula = eliment_data['formula_text']
         try:
             variables = data.copy()
             del eliment_data['formula']
@@ -40,5 +50,5 @@ class ElimentsCount(APIView):
             result = eval(formula)
         except NameError:
             result = 'NameError'
-
-        return Response({'result':result}, status=status.HTTP_200_OK)
+        print(response)
+        return render(request, 'eliment_count.html', {'eliment_info': response, 'result': result})
--- elektronica/views/eliments.py
+++ elektronica/views/eliments.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework import status
+from django.shortcuts import render
 from ..models import (
     Eliments,
 )
@@ -13,4 +14,4 @@ class ElimentsList(APIView):
     def get(self, request:Request):
         eliments = Eliments.objects.all()
         serializer = ElimentsSerializer(eliments, many=True)
-        return Response(serializer.data)
+        return render(request, 'elements.html', {'elements': serializer.data})
